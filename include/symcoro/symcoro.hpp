3a2a9266c70d5a75adb96518148c57a12720ee8d
yanbin <yanb25@mail2.sysu.edu.cn>
2020-08-19 23:14:57 +0800

    add comments

--- include/symcoro/symcoro.hpp
+++ include/symcoro/symcoro.hpp
@@ -1,14 +1,27 @@
+/**
+ * @file main file to define CoroWorker
+ */
 #ifndef SYMCORO_H
 #define SYMCORO_H
 #include <boost/context/continuation.hpp>
 #include <queue>
 #include <string>
+/**
+ * symcoro is the main namespace where everything is in
+ */
 namespace symcoro
 {
-using cc_t = boost::context::continuation;
+using cc_t = boost::context::continuation;  //< continuation
+
+/**
+ * @brief CoroWorker is the worker based on coroutine to schedule @see Job.
+ */
 class CoroWorker
 {
 public:
+    /**
+     * Job represents a job that accepts and returns continuation.
+     */
     using Job = std::function<cc_t(cc_t &&)>;
     CoroWorker() = default;
     CoroWorker(CoroWorker &&rhs);
@@ -17,12 +30,24 @@ public:
     CoroWorker &operator=(const CoroWorker &rhs) = delete;
     ~CoroWorker() = default;
 
+    /**
+     * @brief register a Job to the worker.
+     *
+     * The Job may suspend itself by calling `cc_t.resume()`, and it will
+     * be scheduled to resume later
+     *
+     * @param job the job to register. @see Job
+     */
     void Register(Job job);
+    /**
+     * @brief worker starts to work.
+     *
+     * This invocation blocks until all the jobs have finished
+     */
     void Serve();
 
 private:
     std::queue<cc_t> works_;
 };
-std::string hello();
 }  // namespace symcoro
 #endif
